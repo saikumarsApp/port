bf5b45d89b9fb5a4e7a2a5a04b2d5c6a3ec591ca
saikumarsApp <saikumarnp8@gmail.com>
2024-08-19 12:39:06 +0530

    commit message

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="main-heading">Sai Kumar Bhukya</h1>
+    </div>
+  );
+}
+
+export default App;
